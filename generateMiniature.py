86274a6b222ff07c08ce3efd5d4d20721978a71c
Diego Calvo Alegre <dcalvo@isyc.com>
2023-02-02 16:42:51 +0100

    change rgb to hex

--- generateMiniature.py
+++ generateMiniature.py
@@ -24,7 +24,7 @@ for filename in os.listdir(input_dir):
             colors = []
             for line in colors_file:
                 r, g, b = map(float, line.strip().split())
-                colors.append((b, g, r))
+                colors.append((r, g, b))
 
         # Create an image with one vertical bar for each color
         height = 100
@@ -59,3 +59,71 @@ for filename in os.listdir(input_dir):
             continue
         final_image.save(output_file)
         print(f'{base_name} done')
+'''
+import os
+import numpy as np
+from PIL import Image
+
+path = '/home/dcalvo/code/movie-to-colorimage'
+
+# Create a variable to store the input and output directories
+input_dir = os.path.join(path, 'colors')
+output_dir = os.path.join(path, 'images')
+
+# Make sure the output directory exists
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
+
+# Iterate over all the files in the input directory
+for filename in os.listdir(input_dir):
+    if filename.endswith('.txt'):
+        # Get the base name of the file (without the .txt extension)
+        base_name = os.path.splitext(filename)[0]
+
+        # Open the file with the RGB colors
+        with open(os.path.join(input_dir, filename), 'r') as colors_file:
+            # Read the colors from the file and store them in a list
+            colors = []
+            for line in colors_file:
+                hex_color = line.strip()
+                #r, g, b = tuple(int(hex_color[i:i+2], 16) for i in (0, 2, 4))
+                r, g, b = tuple(int(line[1:][i:i+2], 16) for i in (0, 2, 4))
+
+
+
+                colors.append((r, g, b))
+
+        # Create an image with one vertical bar for each color
+        height = 100
+        width = len(colors)
+        image = np.zeros((height, width, 3), dtype=np.uint8)
+        for i, color in enumerate(colors):
+            image[:, i] = color
+
+        # Open the image with the RGB colors using the Image module
+        img = Image.fromarray(image)
+
+        # Resize the image
+        resized_image = img.resize((1920, 1080))
+
+        # Create a blank image with the desired resolution
+        final_image = Image.new('RGB', (1920, 1080))
+        pixels = final_image.load()
+
+        # Iterate through the pixels and set the color for each pixel
+        for i in range(1920):
+            for j in range(1080):
+                # Get the color for the current pixel from the resized image
+                r, g, b = resized_image.getpixel((i, j))
+                pixels[i, j] = (r, g, b)
+
+        final_image = final_image.transpose(Image.ROTATE_270)
+
+        # Save the modified image if not exists
+        output_file = os.path.join(output_dir, f'{base_name}.jpg')
+        if os.path.isfile(output_file):
+            print(f'{output_file} already exists, skipping...')
+            continue
+        final_image.save(output_file)
+        print(f'{base_name} done')
+'''
---
+++ rgbtohex.py
@@ -0,0 +1,85 @@
+import os
+
+def float_to_hex(float_rgb):
+    int_rgb = [round(x) for x in float_rgb]
+    hex_rgb = ''.join([format(x, '02X') for x in int_rgb])
+    return hex_rgb
+
+
+directory = '/home/dcalvo/code/movie-timeline-colors/files'
+for filename in os.listdir(directory):
+    if filename.endswith('.txt'):
+        with open(os.path.join(directory, filename), 'r') as f:
+            lines = f.readlines()
+        hex_colors = [float_to_hex(
+            [float(x) for x in line.split()]) + '\n' for line in lines]
+        with open(os.path.join(directory, filename), 'w') as f:
+            f.writelines(hex_colors)
+
+'''
+import cv2
+import numpy as np
+import sys
+from progress.bar import Bar
+
+
+def rgb_to_hex(rgb):
+    return '#{:02x}{:02x}{:02x}'.format(int(rgb[0]), int(rgb[1]), int(rgb[2]))
+
+
+# Open the video file
+video_path = 'the+tree+of+life.mp4'
+video = cv2.VideoCapture(video_path)
+
+# Get the total number of frames in the video and the frame rate
+total_frames = int(video.get(cv2.CAP_PROP_FRAME_COUNT))
+frame_rate = video.get(cv2.CAP_PROP_FPS)
+
+# Calculate the total number of seconds in the video
+total_seconds = total_frames / frame_rate
+
+# Open a file to write the average colors to
+colors_file = open('the+tree+of+life.txt', 'w')
+
+# Create a progress bar
+bar = Bar('Processing', max=int(total_seconds), suffix='%(percent)d%%')
+
+# Iterate over each second in the video
+for i in range(int(total_seconds)):
+    # Initialize a list to store the frame colors for this second
+    frame_colors = []
+
+    # Iterate over each frame in this second
+    for j in range(int(frame_rate)):
+        # Read the frame
+        _, frame = video.read()
+
+        # Convert the frame to the BGR color space
+        bgr = frame
+
+        # Calculate the average color of the frame
+        avg_color_per_row = np.average(bgr, axis=0)
+        avg_color = np.average(avg_color_per_row, axis=0)
+
+        # Add the average color to the list of frame colors
+        frame_colors.append(avg_color)
+
+    # Calculate the average color for the second
+    avg_color_per_second = np.average(frame_colors, axis=0)
+
+    # Convert the average color from RGB to hexadecimal
+    avg_color_hex = rgb_to_hex(avg_color_per_second)
+
+    # Write the average color to the file
+    colors_file.write(avg_color_hex + '\n')
+
+    # Update the progress bar
+    bar.next()
+
+# Close the file and video capture
+colors_file.close()
+video.release()
+
+# Finish the progress bar
+bar.finish()
+'''
